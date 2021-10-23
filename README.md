9933b9efbd72cf4693fa768abe61ba31ce5e5b22
RioGruz <mario.mandusic1729@gmail.com>
2021-10-23 15:23:07 +0200

    Nova verzija

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# vlado
+# nova verzija
--- vlado.py
+++ vlado.py
@@ -2,11 +2,21 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
+import json
+from streamlit_lottie import st_lottie
 
 def main():
+
+	def load_lottiefile(filepath: str):
+		with open (filepath, "r") as f:
+			return json.load(f)
+
+	lottie_coding = load_lottiefile("ship.json")
+	st_lottie(lottie_coding, speed=1, reverse=False, loop=True, height=250, width=500)
 	st.title("XGBoost for LightShip prediction")
+
 	# load saved model
-	with open('model_pkl' , 'rb') as f:
+	with open('model_0_pkl' , 'rb') as f:
 		lr = pickle.load(f)
 	LOA = st.number_input("Enter LOA")
 	B = st.number_input("Enter B")
